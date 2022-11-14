f0b973682d5fea716dff31f3a69aedda4fca86f3
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-11-14 18:32:21 +0100

    persist installé

--- argentbank/src/store/store.js
+++ argentbank/src/store/store.js
@@ -1,8 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-export default configureStore({
-  reducer: {
-    userReducer,
-  },
+const reducers = combineReducers({
+  userReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+
+  //devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
+export default store;
