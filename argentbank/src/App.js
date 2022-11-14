bc620c03da30c62114ad1aea0effef9d8db146b0
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-11-14 16:40:18 +0100

    Modif wrap entre index.js et app.js

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,29 +1,24 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
 // import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 import SignIn from './pages/SignIn';
 import Profile from './pages/Profile';
-import { Provider } from 'react-redux';
-import store from '../src/store/store';
 
 function App() {
-	return (
-		<Provider store={store}>
-			<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/signIn' element={<SignIn />} />
-				<Route path='/profile' element={<Profile />} />
-			</Routes>
-		</BrowserRouter>
-		</React.StrictMode>
-		</Provider>
-		
-	);
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signIn' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 }
 
 export default App;
