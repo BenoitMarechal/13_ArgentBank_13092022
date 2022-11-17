ad7429ee7be2f9234e7dd5911bd2b197c7e94bd1
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-11-17 18:02:56 +0100

    Simplif actions OK

--- argentbank/src/store/slices/userSlice.js
+++ argentbank/src/store/slices/userSlice.js
@@ -21,78 +21,78 @@ export const userSlice = createSlice({
     setUser: (state, action) => {
       return { ...state, ...action.payload };
     },
-    setUserEmail: (state, action) => {
-      state.email = action.payload;
-    },
-    setUserId: (state, action) => {
-      state.userId = action.payload;
-    },
+    // setUserEmail: (state, action) => {
+    //   state.email = action.payload;
+    // },
+    // setUserId: (state, action) => {
+    //   state.userId = action.payload;
+    // },
 
-    setUserRemember: (state, action) => {
-      state.remember = action.payload;
-    },
-    setRemember: (state) => {
-      let target = document.getElementById('remember-me');
-      if (target && target.checked === true) {
-        state.remember = true;
-      } else {
-        state.remember = false;
-      }
-    },
+    // setUserRemember: (state, action) => {
+    //   state.remember = action.payload;
+    // },
+    // setRemember: (state) => {
+    //   let target = document.getElementById('remember-me');
+    //   if (target && target.checked === true) {
+    //     state.remember = true;
+    //   } else {
+    //     state.remember = false;
+    //   }
+    // },
 
-    setConnectedTrue: (state) => {
-      state.connected = true;
-    },
-    setConnectedFalse: (state) => {
-      state.connected = false;
-    },
-    toggleConnected: (state) => {
-      state.connected = !state.connected;
-    },
-    setToken: (state, action) => {
-      state.token = action.payload;
-    },
-    setFirstName: (state, action) => {
-      state.firstName = action.payload;
-    },
-    setPassword: (state, action) => {
-      state.password = action.payload;
-    },
-    setLastName: (state, action) => {
-      state.lastName = action.payload;
-    },
+    // setConnectedTrue: (state) => {
+    //   state.connected = true;
+    // },
+    // setConnectedFalse: (state) => {
+    //   state.connected = false;
+    // },
+    // toggleConnected: (state) => {
+    //   state.connected = !state.connected;
+    // },
+    // setToken: (state, action) => {
+    //   state.token = action.payload;
+    // },
+    // setFirstName: (state, action) => {
+    //   state.firstName = action.payload;
+    // },
+    // setPassword: (state, action) => {
+    //   state.password = action.payload;
+    // },
+    // setLastName: (state, action) => {
+    //   state.lastName = action.payload;
+    // },
     resetUser: () => {
       return initialState;
     },
     toggleEdit: (state) => {
       state.editOn = !state.editOn;
     },
-    setUserErrorTrue: (state) => {
-      state.userError = true;
-    },
-    setPasswordErrorTrue: (state) => {
-      state.passwordError = true;
-    },
+    // setUserErrorTrue: (state) => {
+    //   state.userError = true;
+    // },
+    // setPasswordErrorTrue: (state) => {
+    //   state.passwordError = true;
+    //},
   },
 });
 
 export const {
   setUser,
-  setUserEmail,
-  setUserId,
-  setUserRemember,
+  // setUserEmail,
+  // setUserId,
+  // setUserRemember,
   toggleEdit,
-  setUserAll,
-  setRemember,
-  setConnectedTrue,
-  setFirstName,
-  setLastName,
-  setConnectedFalse,
-  setToken,
-  toggleConnected,
-  setPassword,
+  // setUserAll,
+  // setRemember,
+  // setConnectedTrue,
+  // setFirstName,
+  // setLastName,
+  // setConnectedFalse,
+  // setToken,
+  // toggleConnected,
+  // setPassword,
   resetUser,
-  setUserErrorTrue,
-  setPasswordErrorTrue,
+  // setUserErrorTrue,
+  // setPasswordErrorTrue,
 } = userSlice.actions;
 export default userSlice.reducer;
